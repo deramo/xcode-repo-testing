7ac235319219c3113801927b07174f12d0abe1aa
Dominic <dominic@doms-macbook.local>
2016-05-25 14:50:58 -0400

    view controller update

--- testing/ViewController.swift
+++ testing/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import AudioToolbox
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textLabel: UILabel!
+    let session = AVCaptureSession()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textLabel.text = nil
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonWasPushed(sender: UIButton) {
+        
+        self.textLabel.text = "it works"
+        
+    }
+
+    @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+        
+        self.textLabel.text = nil
+    
+    }
+    
+    @IBAction func didLongPress(sender: UILongPressGestureRecognizer) {
+        
+        self.textLabel.text = "Hey! " + "let go of ime"
+        AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+        
+
+    }
 
 }
 
